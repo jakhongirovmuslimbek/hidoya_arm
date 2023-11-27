dd84005da9026b2acd3e91828d122b0568b2c582
jakhongirovmuslimbek <jmuslimbek2001@gmail.com>
2023-11-27 17:49:12 +0500

    commit

--- core/settings.py
+++ core/settings.py
@@ -115,7 +115,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
@@ -130,17 +129,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-MEDIA_URL = "/media/"
-STATIC_ROOT = BASE_DIR / "static"
-# STATICFILES_DIRS = [BASE_DIR / 'static']
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-# MEDIA_ROOT = '/home/hidoya/hidoya_arm/media'
-# MEDIA_URL = '/media/'
-# STATIC_ROOT = '/home/hidoya/hidoya_arm/static'
 # STATIC_URL = '/static/'
+# MEDIA_URL = "/media/"
+# STATIC_ROOT = BASE_DIR / "static"
+# # STATICFILES_DIRS = [BASE_DIR / 'static']
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+MEDIA_ROOT = '/home/hidoya/hidoya_arm/media'
+MEDIA_URL = '/media/'
+STATIC_ROOT = '/home/hidoya/hidoya_arm/static'
+STATIC_URL = '/static/'
 
 
 # Default primary key field type
